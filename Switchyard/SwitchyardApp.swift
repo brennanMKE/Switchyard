a833ed61a6e3c2fc03016c4da38aa4325c376501
Brennan Stehling <277419+brennanMKE@users.noreply.github.com>
2026-08-06 12:45:20 -0700

    Initial Commit

---
+++ Switchyard/SwitchyardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchyardApp.swift
+//  Switchyard
+//
+//  Created by Brennan Stehling on 8/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SwitchyardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwitchyardTests/SwitchyardTests.swift
@@ -0,0 +1,18 @@
+//
+//  SwitchyardTests.swift
+//  SwitchyardTests
+//
+//  Created by Brennan Stehling on 8/6/26.
+//
+
+import Testing
+
+struct SwitchyardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
